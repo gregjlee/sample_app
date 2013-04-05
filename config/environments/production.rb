52f79958e853cf9893860e66586b59a730a1ba37
gregjlee <mail.greglee@gmail.com>
2013-04-05 11:34:56 -0700

    add ssl in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,6 +20,9 @@ SampleApp::Application.configure do
   # Generate digests for assets URLs
   config.assets.digest = true
 
+  #force ssl
+  config.force_ssl=true
+
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
 
