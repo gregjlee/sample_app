fa1bfc72939e86d1cd93ef2686b818be18c1d235
gregjlee <mail.greglee@gmail.com>
2013-04-05 17:53:05 -0700

    Finish sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  include SessionsHelper
+
+  #force signout to prevent csrf attacks
+  def handle_unverified_requests
+  	sign_out
+  	super
+  end
 end
